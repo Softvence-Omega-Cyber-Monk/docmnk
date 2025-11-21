11c929d92f83aad160bd4ab9b85343a3643da05d
SouravChowdhuryRimel <souravchowdhury6519@gmail.com>
2025-11-21 16:19:15 +0600

    update user management

--- src/app/modules/userManagements/userManagement.model.ts
+++ src/app/modules/userManagements/userManagement.model.ts
@@ -10,6 +10,7 @@ const CompanyInformationSchema = new Schema<ICompanyInformation>(
     role: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phoneNumber: { type: String, required: true },
+    imageUrl: { type: String }, // Optional field
     password: { type: String, required: true },
     isReferred: { type: Boolean, default: false },
   },
