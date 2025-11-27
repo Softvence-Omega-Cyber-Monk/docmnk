33f44b6ec3efea6c516f9f9b94c182a927b20be7
SouravChowdhuryRimel <souravchowdhury6519@gmail.com>
2025-11-27 20:04:52 +0600

    update camp

--- src/app/modules/eventManagements/eventManagement.route.ts
+++ src/app/modules/eventManagements/eventManagement.route.ts
@@ -9,5 +9,7 @@ router.get("/getall", EventManagementController.getAllCamps);
 router.get("/getSingle/:id", EventManagementController.getSingleCamp);
 router.put("/update/:id", EventManagementController.updateCamp);
 router.delete("/deletCamp/:id", EventManagementController.deleteCamp);
+// GET nearby camps
+router.get("/nearby-camps", EventManagementController.getNearbyCampsController);
 
 export const eventManagementRouter = router;
